a4de1d074e82945dfef326e4134ba36a83ab2f0c
lalitharw <lalitharwate90@gmail.com>
2026-06-25 21:05:27 +0530

    authentication utils updated --lalit

--- src/utils/api.jsx
+++ src/utils/api.jsx
@@ -20,4 +20,18 @@ api.interceptors.request.use((config) => {
     }
 )
 
+
+api.interceptors.response.use((response) => {
+    return response
+},
+    (error) => {
+        if (error.response && error.response.status === 401) {
+            localStorage.removeItem("token")
+            window.location.href = "/login"
+        }
+
+        return Promise.reject(error)
+    }
+)
+
 export default api
